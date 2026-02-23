46dfb59f8f93eb6e398a4278fd66a25752d7e5e5
Matheus <matheusjosedasilva@live.com>
2026-02-22 21:29:46 -0300

    Initial Commit

---
+++ CameraSamsung/CameraSamsungApp.swift
@@ -0,0 +1,32 @@
+//
+//  CameraSamsungApp.swift
+//  CameraSamsung
+//
+//  Created by Matheus José on 22/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CameraSamsungApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
